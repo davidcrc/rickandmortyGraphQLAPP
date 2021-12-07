b63fc4cced1a82080561b8f286945a9e5f02a000
davidcrc <david_drcr@hotmail.com>
2021-12-07 15:27:16 -0500

    initial

---
+++ src/app/graphql.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+
+const uri = '.';
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<unknown> {
+  return {
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule { }
